6bff7f47b82bdeb1c1e17f33b3c84175e3da015b
galezzza <121448649+galezzza@users.noreply.github.com>
2023-01-14 11:02:43 +0100

    Update cat_recognition.py

--- cat_recognition.py
+++ cat_recognition.py
@@ -6,26 +6,21 @@ class CatRecognizer:
     def is_cat(self, image_path: str) -> bool:
         model = keras.applications.VGG16()
 
-        img_dir = image_path
-
-        img = cv2.imread(img_dir)
+        img = cv2.imread(image_path)
         img = cv2.resize(img, (224, 224))
         img = np.array(img)
+
         x = keras.applications.vgg16.preprocess_input(img)
         x = np.expand_dims(x, axis=0)
 
         res = model.predict(x)
         res = np.argmax(res)
 
-        if 281 <= res <= 285:
-            return True
-        else:
-            return False
-
-        raise NotImplementedError
+        # VGG16 is based on imagenet dataset, in this dataset the cat IDs range from 281 to 285
+        return 281 <= res <= 285
 
 
 # img_dir = 'image_dir/photo_2022-12-18_15-03-48.jpg'
 
 # CatRecognizer1 = CatRecognizer()
-# print(CatRecognizer1.is_cat('image_dir/HERE_PUT_IMAGE_AND_NAME_IT_LIKE_THIS.jpeg'))
+# print(CatRecognizer1.is_cat('image_dir/photo_2022-12-18_15-03-48.jpg'))
