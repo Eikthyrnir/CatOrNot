34ac7ebdf6e4ad7d1dd584130dc0966cc19fb85f
galezzza <121448649+galezzza@users.noreply.github.com>
2023-01-14 10:35:10 +0100

    Update cat-recognition.py

--- cat-recognition.py
+++ cat-recognition.py
@@ -2,15 +2,13 @@ from tensorflow import keras
 import numpy as np
 import cv2
 
-
-class Recognation:
-    def __init__(self, picture):
-        self.picture = picture
-
-    def recognize(self):
+class CatRecognizer:
+    def is_cat(self, image_path: str) -> bool:
         model = keras.applications.VGG16()
 
-        img = self.picture
+        img_dir = image_path
+
+        img = cv2.imread(img_dir)
         img = cv2.resize(img, (224, 224))
         img = np.array(img)
         x = keras.applications.vgg16.preprocess_input(img)
@@ -24,13 +22,10 @@ class Recognation:
         else:
             return False
 
+        raise NotImplementedError
 
-# img_dir = 'image_dir/dgwesdgfs.jpeg'
-#
-# img = cv2.imread(img_dir)
-# Recognation1 = Recognation(img)
-#
-# if Recognation1.recognize() == True:
-#     print(0)
-# else:
-#     print(1)
+
+# img_dir = 'image_dir/photo_2022-12-18_15-03-48.jpg'
+
+# CatRecognizer1 = CatRecognizer()
+# print(CatRecognizer1.is_cat('image_dir/HERE_PUT_IMAGE_AND_NAME_IT_LIKE_THIS.jpeg'))
