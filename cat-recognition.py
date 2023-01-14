3e56cf857d005b650b3a3a3f34a37a117ac2e46d
galezzza <121448649+galezzza@users.noreply.github.com>
2023-01-14 10:13:40 +0100

    Update cat-recognition.py

--- cat-recognition.py
+++ cat-recognition.py
@@ -2,24 +2,35 @@ from tensorflow import keras
 import numpy as np
 import cv2
 
-model = keras.applications.VGG16()
 
-img_dir = 'image_dir/dgwesdgfs.jpeg'
+class Recognation:
+    def __init__(self, picture):
+        self.picture = picture
+
+    def recognize(self):
+        model = keras.applications.VGG16()
+
+        img = self.picture
+        img = cv2.resize(img, (224, 224))
+        img = np.array(img)
+        x = keras.applications.vgg16.preprocess_input(img)
+        x = np.expand_dims(x, axis=0)
+
+        res = model.predict(x)
+        res = np.argmax(res)
+
+        if 281 <= res <= 285:
+            return True
+        else:
+            return False
 
 
-def CATorNOT(img_dir):
-    img = cv2.imread(img_dir)
-    img = cv2.resize(img, (224, 224))
-    img = np.array(img)
-    x = keras.applications.vgg16.preprocess_input(img)
-    x = np.expand_dims(x, axis=0)
-
-    res = model.predict(x)
-    res = np.argmax(res)
-
-    if 281 <= res <= 285:
-        return True
-    else:
-        return False
-
-print(CATorNOT(img_dir))
+# img_dir = 'image_dir/dgwesdgfs.jpeg'
+#
+# img = cv2.imread(img_dir)
+# Recognation1 = Recognation(img)
+#
+# if Recognation1.recognize() == True:
+#     print(0)
+# else:
+#     print(1)
